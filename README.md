8b13ae61f1408746461cb4a90778ccc8b180cf0b
Gendo Studio <gendosoft@gmail.com>
2023-04-19 00:47:12 +0700

    Init

---
+++ README.md
@@ -0,0 +1,32 @@
+dockerize-laravel-5
+#####
+
+
+## How to use it
+
+1. Clone this repository:
+
+```bash
+git clone 
+```
+
+2. Make a copy of our `.env.sample` and rename it to `.env`:
+
+Update this file with your preferences.
+
+```
+```
+
+```
+chmod +rx start.sh
+./start.sh
+```
+
+
+
+## Down
+```
+docker-compose down
+docker network remove app_network
+rm -rf .data/*
+```
---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#
+# This file should be used to prepare and run your laravel project use apache after set up your .env file
+# Source: https://github.com/
+#
+
+# 1. Check if .env file exists
+if [ -e .env ]; then
+    source .env
+else
+    echo "It seems you didn´t create your .env file, so we will create one for you."
+    cp .env.example .env
+    # exit 1
+fi
+
+# 1. Check if .env file exists
+if [[ -e opts/sites-available/app.conf ]]; then
+    cp opts/sites-available/app.conf.example opts/sites-available/app.conf
+fi
+
+# 2. Create docker network
+docker network create $NETWORK
+
+# 3. Update local images
+docker compose pull
+
+# 6. Add any special configuration if it's set in .env file
+
+
+# 7. Start proxy
+docker compose up -d --build
+
+echo "You web is setting up at http://localhost:$APP_PORT"
+
+exit 0
